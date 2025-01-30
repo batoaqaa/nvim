8c45f9a7b7bd6ea90d8993ba643c85f2f8e120bd
batoaqaa <ahmed.batoaq@gmail.com>
2025-01-30 20:59:46 +0300

    update

--- lua/kamailio.lua
+++ lua/kamailio.lua
@@ -1,4 +1,5 @@
---https://ibrahimshahzad.github.io/posts/writing_lsp_for_kamailio_cfg_p1/
+--[[https://ibrahimshahzad.github.io/posts/writing_lsp_for_kamailio_cfg_p1/]]
+--[[https://github.com/IbrahimShahzad/kamaizen.nvim/tree/master]]
 vim.api.nvim_create_autocmd('FileType', {
   --pattern = 'kamailio_cfg',
   pattern = 'cfg',
