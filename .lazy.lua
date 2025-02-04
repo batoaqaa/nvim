0db2d4bbbe1552588fdba7e04c42f264d26eb7d5
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-04 08:32:33 +0300

    update

---
+++ .lazy.lua
@@ -0,0 +1,6 @@
+-- .lazy.lua file to set project-specific configurations without changing your dotfiles
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.g.autoformat = false
+
+return {}
--- lua/kickstart/plugins/kamailio.lua
+++ lua/kickstart/plugins/kamailio.lua
@@ -1,5 +1,8 @@
 return {
   'batoaqaa/kamailio-nvim', -- replace this with your {user_name}/{repo_name}
+  dependencies = {
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+  },
   opts = {
     settings = {
       kamaizen = {
