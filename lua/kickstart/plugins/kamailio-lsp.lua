d7fb645e2e5fc8aee4de5282391b0b6f29827570
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-24 22:13:32 +0300

    Update

--- lua/kickstart/plugins/kamailio-lsp.lua
+++ lua/kickstart/plugins/kamailio-lsp.lua
@@ -1,15 +1,32 @@
+-- return {
+--   'batoaqaa/kamailio-lsp',
+--   -- 'batoaqaa/kamaizen.nvim',
+--   dependencies = {
+--     { 'IbrahimShahzad/KamaiZen', build = 'go build' },
+--   },
+--   -- lazy = true,
+--   opts = {
+--     settings = {
+--       kamaizen = {
+--         enableDeprecatedCommentHint = false, -- to enable hints for '#' comments
+--         enableDiagnostics = true, -- to enable/disable diagnostics
+--         KamailioSourcePath = vim.fn.getcwd(),
+--         loglevel = 3,
+--       },
+--     },
+--   },
+-- }
 return {
-  'batoaqaa/kamailio-lsp',
-  -- 'batoaqaa/kamaizen.nvim',
-  dependencies = {
-    { 'IbrahimShahzad/KamaiZen', build = 'go build' },
-  },
-  -- lazy = true,
+  'IbrahimShahzad/KamaiZen',
+  -- 'batoaqaa/KamaiZen',
+  branch = 'master', -- or tag = 'v0.0.5'
+  build = 'go build',
   opts = {
     settings = {
       kamaizen = {
         enableDeprecatedCommentHint = false, -- to enable hints for '#' comments
-        KamailioSourcePath = vim.fn.getcwd(),
+        enableDiagnostics = true, -- to enable/disable diagnostics
+        KamailioSourcePath = '/path/to/kamailio', -- or use current dir vim.fn.getcwd()
         loglevel = 3,
       },
     },
