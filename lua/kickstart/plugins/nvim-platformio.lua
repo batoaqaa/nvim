143253920479bb1ba9c684b56a9d84493796efd6
batoaqaa <ahmed.batoaq@gmail.com>
2025-03-08 12:50:02 +0300

    Update

--- lua/kickstart/plugins/nvim-platformio.lua
+++ lua/kickstart/plugins/nvim-platformio.lua
@@ -1,15 +1,18 @@
 return {
-  'batoaqaa/nvim-platformio.lua',
-  -- 'anurag3301/nvim-platformio.lua',
+  -- 'batoaqaa/nvim-platformio.lua',
+  'anurag3301/nvim-platformio.lua',
   lazy = false,
   dependencies = {
     { 'akinsho/nvim-toggleterm.lua' },
     { 'nvim-telescope/telescope.nvim' },
     { 'nvim-lua/plenary.nvim' },
   },
-  -- config = function()
-  --   require('platformio.nvim').setup {}
-  -- end,
+  config = function()
+    require('platformio').setup({
+      lsp = 'clangd', --default: ccls, other option: clangd
+      -- If you pick clangd, it also creates compile_commands.json
+    })
+  end,
   -- opts = {},
   cmd = {
     'Pioinit',
