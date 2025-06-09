8b441c319bbec6cb7861ad4ef72b90a11826a03e
batoaqaa <ahmed.batoaq@gmail.com>
2025-06-09 23:23:24 +0300

    Update

--- lua/pynvim.lua
+++ lua/pynvim.lua
@@ -1,15 +1,11 @@
-local python_version = 'python3.13'
+local python_version = 'python3.12'
 --
---global environment variable PLATFORMIO_CORE_DIR
---    Unix ~/.platformio
---    Windows %HOMEPATH%\.platformio
-
 --:lua os.execute("setx PLATFORMIO_CORE_DIR %HOMEPATH%/.platformio")
 --:lua os.execute("setx PLATFORMIO_CORE_DIR %userprofile%/.platformio")
 --:lua print(os.getenv("userprofile"))
 local platformio_core_dir, pynvim_env, pynvim_python, pynvim_lib, pynvim_bin, pynvim_activate
 if jit.os == 'Windows' then
-  platformio_core_dir = os.getenv('userprofile') .. '\\.platformio'
+  platformio_core_dir = vim.env.HOME .. '\\.platformio'
 
   pynvim_env = platformio_core_dir .. '/nenv'
   pynvim_bin = pynvim_env .. '\\Scripts'
@@ -17,24 +13,35 @@ if jit.os == 'Windows' then
   pynvim_activate = pynvim_bin .. '\\activate'
 
   pynvim_lib = pynvim_env .. '\\Lib\\' .. '\\site-packages\\pynvim'
+  os.execute('setx PLATFORMIO_CORE_DIR ' .. platformio_core_dir)
 else
-  platformio_core_dir = '~/.platformio'
+  platformio_core_dir = vim.env.HOME .. '/.platformio'
 
   pynvim_env = platformio_core_dir .. '/nenv'
   pynvim_bin = pynvim_env .. '/bin'
   pynvim_python = pynvim_bin .. '/python'
-  pynvim_activate = 'source' .. pynvim_bin .. '/activate'
+  pynvim_activate = pynvim_bin .. '/activate'
 
   pynvim_lib = pynvim_env .. '/lib/' .. python_version .. '/site-packages/pynvim'
+  vim.uv.os_setenv('PLATFORMIO_CORE_DIR', platformio_core_dir)
 end
 
 if vim.fn.isdirectory(platformio_core_dir) == 0 then
-  os.execute('mkdir ' .. platformio_core_dir)
+  vim.fn.mkdir(platformio_core_dir, 'p')
+  os.execute('wget https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py')
+  os.execute('python3 get-platformio.py')
 end
-os.execute('setx PLATFORMIO_CORE_DIR ' .. platformio_core_dir)
 
+-- local expand_dir = vim.fn.expand(pynvim_env)
 if not vim.uv.fs_stat(pynvim_env) then
   vim.fn.system({ 'python', '-m', 'venv', pynvim_env })
+end
+
+print(vim.uv.os_getenv('PLATFORMIO_CORE_DIR'))
+print(vim.uv.os_getenv('PATH'))
+
+if jit.os ~= 'Windows' then
+  os.execute('chmod 755 -R ' .. pynvim_bin)
 end
 
 if not vim.uv.fs_stat(pynvim_lib) then
