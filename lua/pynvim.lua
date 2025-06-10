51d41c9b196788eb00491a6d0996ca4693da9ad3
batoaqaa <ahmed.batoaq@gmail.com>
2025-06-10 18:12:59 +0300

    Update

--- lua/pynvim.lua
+++ lua/pynvim.lua
@@ -1,68 +1,46 @@
-local python_version = 'python3.13'
+local isWindows = jit.os == 'Windows'
 --
---:lua os.execute("setx PLATFORMIO_CORE_DIR %HOMEPATH%/.platformio")
---:lua os.execute("setx PLATFORMIO_CORE_DIR %userprofile%/.platformio")
---:lua print(os.getenv("userprofile"))
 local platformio_core_dir, pynvim_env, pynvim_python, pynvim_lib, pynvim_bin, pynvim_activate
-if jit.os == 'Windows' then
+if isWindows then
   platformio_core_dir = vim.env.HOME .. '\\.platformio'
-
-  pynvim_env = platformio_core_dir .. '/nenv'
+  pynvim_env = platformio_core_dir .. '\\nenv'
   pynvim_bin = pynvim_env .. '\\Scripts'
   pynvim_python = pynvim_bin .. '\\python.exe'
   pynvim_activate = pynvim_bin .. '\\activate'
-
-  -- pynvim_lib = pynvim_env .. '\\Lib\\' .. '\\site-packages\\pynvim'
-  -- os.execute('setx PLATFORMIO_CORE_DIR ' .. platformio_core_dir)
 else
   platformio_core_dir = vim.env.HOME .. '/.platformio'
-
   pynvim_env = platformio_core_dir .. '/nenv'
   pynvim_bin = pynvim_env .. '/bin'
   pynvim_python = pynvim_bin .. '/python'
   pynvim_activate = pynvim_bin .. '/activate'
-
-  -- pynvim_lib = pynvim_env .. '/lib/' .. python_version .. '/site-packages/pynvim'
 end
 
 vim.uv.os_setenv('PLATFORMIO_CORE_DIR', platformio_core_dir)
+vim.g.python_host_prog = pynvim_python
+vim.g.python3_host_prog = pynvim_python
+vim.env.PATH = pynvim_bin .. (isWindows and ';' or ':') .. vim.env.PATH
+vim.env.VIRTUAL_ENV = pynvim_env
+-- print(vim.env.PATH)
+
 if vim.fn.isdirectory(platformio_core_dir) == 0 then
   vim.fn.mkdir(platformio_core_dir, 'p')
   vim.fn.system({ 'wget', 'https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py' })
   vim.fn.system({ 'python', 'get-platformio.py' })
-  if jit.os == 'Windows' then
-    os.execute('del get-platformio.py*')
-  else
-    os.execute('rm -f get-platformio.py*')
-  end
+  os.execute((isWindows and 'del ' or 'rm -f ') .. 'get-platformio.py*')
 end
 
 -- local expand_dir = vim.fn.expand(pynvim_env)
 if not vim.uv.fs_stat(pynvim_env) then
   vim.fn.system({ 'python', '-m', 'venv', pynvim_env })
-  if jit.os ~= 'Windows' then
+  if not isWindows then
     os.execute('chmod 755 -R ' .. pynvim_bin)
   end
 
-  vim.g.python_host_prog = pynvim_python
-  vim.g.python3_host_prog = pynvim_python
-  vim.env.PATH = pynvim_bin .. ':' .. vim.env.PATH
-  vim.env.VIRTUAL_ENV = pynvim_env
-
-  -- if not vim.uv.fs_stat(pynvim_lib) then
   vim.fn.system({ pynvim_python, '-m', 'pip', 'install', 'pynvim' })
   vim.fn.system({ pynvim_python, '-m', 'pip', 'install', 'neovim' })
   vim.fn.system({ pynvim_python, '-m', 'pip', 'install', 'debugpy' })
   vim.fn.system({ pynvim_python, '-m', 'pip', 'install', 'isort' })
   vim.fn.system({ pynvim_python, '-m', 'pip', 'install', 'scons' })
   vim.fn.system({ pynvim_python, '-m', 'pip', 'install', 'yamllint' })
-  -- end
 end
-
--- vim.fn.system({ pynvim_activate })
-
--- uv.os_getenv({name} [, {size}])                                 *uv.os_getenv()*
--- uv.os_setenv({name}, {value})                                   *uv.os_setenv()*
--- uv.os_unsetenv({name})                                        *uv.os_unsetenv()*
--- uv.os_environ()                                                *uv.os_environ()*
 ------------------------
